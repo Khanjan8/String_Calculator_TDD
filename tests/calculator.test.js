fc7d9aba36193be02bcc568ee2320a64e6055c00
Khanjan8 <khanjanfadadu@gmail.com>
2025-07-04 22:03:20 +0530

    handled single number input

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,3 +3,7 @@ const { add } = require('../src/calculator');
 test('returns 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns number when a single number is passed', () => {
+  expect(add("1")).toBe(1);
+});
