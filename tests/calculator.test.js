6c4466fcae8ae15ff3c7f944dd13a6c49e50573e
Khanjan8 <khanjanfadadu@gmail.com>
2025-07-04 22:11:54 +0530

    handled multiple comma seperated numbers

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -11,3 +11,7 @@ test('returns number when a single number is passed', () => {
 test('returns sum of two comma-separated numbers', () => {
   expect(add("1,2")).toBe(3);
 });
+
+test('returns sum of any number of comma-separated numbers', () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
