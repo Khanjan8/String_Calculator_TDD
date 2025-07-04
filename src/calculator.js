d5b44fecfe0bddcd95b3729d010f892b5ac9a54f
Khanjan8 <khanjanfadadu@gmail.com>
2025-07-04 22:40:49 +0530

    handled newline and custom as delimiters

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,16 @@ function add(numbers) {
   if (numbers === "") return 0;
   if (!numbers.includes(',')) return parseInt(numbers);
 
-  const parts = numbers.split(',');
+  let delimiter = /,|\n/;
+
+  if (numbers.startsWith("//")) 
+  {
+    const parts = numbers.split('\n');
+    delimiter = new RegExp(parts[0][2]);
+    numbers = parts[1];
+  }
+
+  const parts = numbers.split(delimiter);
   return parts.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -15,3 +15,11 @@ test('returns sum of two comma-separated numbers', () => {
 test('returns sum of any number of comma-separated numbers', () => {
   expect(add("1,2,3,4")).toBe(10);
 });
+
+test('handles newlines as valid delimiters', () => {
+  expect(add("1\n2,3")).toBe(6);
+});
+
+test('supports custom delimiter ;', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
